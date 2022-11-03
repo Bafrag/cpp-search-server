b44931e638aba05d451acaa6caf7f6fde54c5bec
Bafrag <83549995+Bafrag@users.noreply.github.com>
2022-11-03 12:10:14 +0300

    Update read_input_functions.cpp

--- search-server/read_input_functions.cpp
+++ search-server/read_input_functions.cpp
@@ -1,6 +1,5 @@
 #include "read_input_functions.h"
 
-#include "document.h"
 using namespace std;
 
 string ReadLine() {
@@ -14,30 +13,4 @@ int ReadLineWithNumber() {
     cin >> result;
     ReadLine();
     return result;
-}
-
-void PrintDocument(const Document& document) {
-    cout << "{ "s
-         << "document_id = "s << document.id << ", "s
-         << "relevance = "s << document.relevance << ", "s
-         << "rating = "s << document.rating << " }"s << endl;
-}
-
-void PrintMatchDocumentResult(int document_id, const vector<string>& words, DocumentStatus status) {
-    cout << "{ "s
-         << "document_id = "s << document_id << ", "s
-         << "status = "s << static_cast<int>(status) << ", "s
-         << "words ="s;
-    for (const string& word : words) {
-        cout << ' ' << word;
-    }
-    cout << "}"s << endl;
-}
-
-ostream& operator<<(ostream& out, const Document& document) {
-    out << "{ "s
-        << "document_id = "s << document.id << ", "s
-        << "relevance = "s << document.relevance << ", "s
-        << "rating = "s << document.rating << " }"s;
-    return out;
 }
