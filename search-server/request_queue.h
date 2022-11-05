70acb88f6bc0494da1206fd606a543160af60264
Bafrag <83549995+Bafrag@users.noreply.github.com>
2022-11-05 18:06:02 +0300

    Update request_queue.h

--- search-server/request_queue.h
+++ search-server/request_queue.h
@@ -10,7 +10,9 @@ public:
     std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate);
 
     std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentStatus status);
+    
     std::vector<Document> AddFindRequest(const std::string& raw_query);
+    
     int GetNoResultRequests() const;
 private:
     struct QueryResult {
@@ -27,7 +29,7 @@ private:
 };
 
 template <typename DocumentPredicate>
-std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate) {
+std::vector<Document> RequestQueue::AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate) {
     const auto result = search_server_.FindTopDocuments(raw_query, document_predicate);
     AddRequest(result.size());
     return result;
