d4ed26d376ce02b961ab92a78acb7363d07e8572
Bafrag <83549995+Bafrag@users.noreply.github.com>
2022-11-05 18:05:10 +0300

    Update paginator.h

--- search-server/paginator.h
+++ search-server/paginator.h
@@ -33,6 +33,15 @@ private:
 };
 
 template <typename Iterator>
+std::ostream& operator<<(std::ostream& out, const IteratorRange<Iterator>& range) {
+    using namespace std::literals;
+    for (Iterator it = range.begin(); it != range.end(); ++it) {
+        out << *it;
+    }
+    return out;
+}
+
+template <typename Iterator>
 class Paginator {
 public:
     Paginator(Iterator begin, Iterator end, size_t page_size) {
