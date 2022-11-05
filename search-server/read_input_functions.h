6da6637897825cc36a88977feab676e9de5cb8ef
Bafrag <83549995+Bafrag@users.noreply.github.com>
2022-11-05 18:04:50 +0300

    Update read_input_functions.h

--- search-server/read_input_functions.h
+++ search-server/read_input_functions.h
@@ -5,12 +5,3 @@
 std::string ReadLine();
 
 int ReadLineWithNumber();
-
-template <typename Iterator>
-std::ostream& operator<<(std::ostream& out, const IteratorRange<Iterator>& range) {
-    using namespace std::literals;
-    for (Iterator it = range.begin(); it != range.end(); ++it) {
-        out << *it;
-    }
-    return out;
-}
