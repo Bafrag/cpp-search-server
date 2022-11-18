23f44aead94cd2007c8ca1c734cf52f283c8d082
Bafrag <83549995+Bafrag@users.noreply.github.com>
2022-11-18 08:48:14 +0300

    Update remove_documents.cpp

--- search-server/remove_documents.cpp
+++ search-server/remove_documents.cpp
@@ -6,16 +6,15 @@ void RemoveDuplicates(SearchServer& search_server) {
     set<int> duplicates;
     for (const int document_id : search_server) {
         if (!duplicates.count(document_id)) {
-            auto test_content = search_server.GetDocumentsWordsWrequencies().at(document_id);
-            for (const auto& [id, content]: search_server.GetDocumentsWordsWrequencies()) {
+            map<string, int> test_content = search_server.GetDocumentsWordsFrequencies().at(document_id);
+            for (const auto& [id, content] : search_server.GetDocumentsWordsFrequencies()) {
                 if (content == test_content && id != document_id) {
                     duplicates.insert(id);
                 }
             }
         }
-        
     }
-    for (int duplicat: duplicates) {
+    for (int duplicat : duplicates) {
         search_server.RemoveDocument(duplicat);
         cout << "Found duplicate document id "s << duplicat << endl;
     }
