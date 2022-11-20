691b65afa25dc9de071d0b9c2337eb7f67a8fadb
Bafrag <83549995+Bafrag@users.noreply.github.com>
2022-11-20 18:13:20 +0300

    Update remove_documents.cpp

--- search-server/remove_documents.cpp
+++ search-server/remove_documents.cpp
@@ -6,13 +6,13 @@ void RemoveDuplicates(SearchServer& search_server) {
     set<int> duplicates;
     set<set<string>> unique;
     for (const int document_id : search_server) {
-        map<string, double> test = search_server.GetWordFrequencies(document_id);
-        set<string> h;
-		for (auto [word, frec] : test) {
-            h.insert(word);
+        map<string, double> words_and_frec_in_document = search_server.GetWordFrequencies(document_id);
+        set<string> set_of_unique_words_in_document;
+		for (auto [word, frec] : words_and_frec_in_document) {
+            set_of_unique_words_in_document.insert(word);
         }
-        if (unique.find(h) == unique.end()) {
-            unique.insert(h);
+        if (unique.find(set_of_unique_words_in_document) == unique.end()) {
+            unique.insert(set_of_unique_words_in_document);
         } else {
             duplicates.insert(document_id);
         }
